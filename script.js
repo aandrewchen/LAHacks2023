f299cb8252b4677686db86678cc70979ea738c7f
Andrew Chen <andrewchen@Andrews-MacBook-Air-7.local>
2023-04-23 04:58:06 -0700

    DONE

--- script.js
+++ script.js
@@ -2,12 +2,11 @@ var swiper = new Swiper(".slide-content", {
     slidesPerView: 3,
     spaceBetween: 30,
     slidesPerGroup: 3,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
     loop: true,
     loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     navigation: {
       nextEl: ".swiper-button-next",
       prevEl: ".swiper-button-prev",
